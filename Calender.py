d6b350bd77b308bd2513838705a34dc9fe79b0c3
2019182029_이원준 <S159753S@tukorea.ac.kr>
2024-05-21 01:02:48 +0900

    클래스를 모듈로 분리
    - 달력을 클래스로 분리 및 프레임, 달력을 별도의 모듈로 분리
    - 프레임 전환을 show(), hide() 함수를 호출하도록 수정
    - 현재 프레임과 목표 프레임이 동일하다면 show(), hide() 함수를 호출하지 않도록 수정
    - 각 프레임에서 두 개의 달력 객체를 갖도록 수정
    - 잡다한 오타 및 변수명 수정

---
+++ Calender.py
@@ -0,0 +1,114 @@
+from tkinter import *
+import calendar
+
+
+class Calender:
+    def __init__(self, parent):
+        self.date_selector_frame = Frame(parent)
+        self.date_selector_frame.pack(side=LEFT, padx=20, pady=10)
+
+        self.selected_date_label = Label(self.date_selector_frame, width=10, text="시작일: 없음", font=("Arial", 10))
+        self.selected_date_label.pack(side=LEFT, padx=10)
+
+        select_date_button = Button(self.date_selector_frame, text="날짜 선택", command=lambda: self.open_date_popup())
+        select_date_button.pack(side=LEFT)
+
+    def open_date_popup(self):
+        self.year = 2024
+        self.month = 1
+
+        self.window = Toplevel()
+        self.window.title("날짜 선택")
+
+        # 년도와 월 선택
+        control_frame = Frame(self.window)
+        control_frame.pack(fill=X)
+
+        year_left_button = Button(control_frame, text="<", command=lambda: self.update_calendar(-1, 0))
+        year_left_button.pack(side=LEFT)
+
+        self.year_label = Label(control_frame, text=str(self.year), font=("Arial", 10))
+        self.year_label.pack(side=LEFT, pady=10)
+
+        year_right_button = Button(control_frame, text=">", command=lambda: self.update_calendar(1, 0))
+        year_right_button.pack(side=LEFT)
+
+        month_left_button = Button(control_frame, text="<", command=lambda: self.update_calendar(0, -1))
+        month_left_button.pack(side=LEFT)
+
+        self.month_label = Label(control_frame, text=str(self.month), font=("Arial", 10))
+        self.month_label.pack(side=LEFT, pady=10)
+
+        month_right_button = Button(control_frame, text=">", command=lambda: self.update_calendar(0, 1))
+        month_right_button.pack(side=LEFT)
+
+        # 달력 생성
+        self.calendar_frame = Frame(self.window)
+        self.calendar_frame.pack(fill=BOTH, expand=True)
+
+        self.create_calendar()
+
+        select_button = Button(self.window, text="선택", command=lambda: self.select_date(self.window))
+        select_button.pack()
+
+    def update_calendar(self, year_delta, month_delta):
+        self.year += year_delta
+        self.month += month_delta
+
+        if self.month > 12:
+            self.month = 1
+            self.year += 1
+        elif self.month < 1:
+            self.month = 12
+            self.year -= 1
+
+        self.year_label.config(text=str(self.year))
+        self.month_label.config(text=str(self.month))
+
+        self.create_calendar()
+
+    def create_calendar(self):
+        for widget in self.calendar_frame.winfo_children():
+            widget.destroy()
+
+        cal = calendar.Calendar()
+        dates = cal.monthdatescalendar(self.year, self.month)
+
+        for week in dates:
+            week_frame = Frame(self.calendar_frame)
+            week_frame.pack(fill=X)
+
+            for day in week:
+                self.create_day_button(week_frame, day)
+
+    def create_day_button(self, parent, day):
+        day_button = Button(
+            parent, text=day.day,
+            width=4, height=2,  # 픽셀 단위로 버튼 크기 지정
+            state=NORMAL if day.month == self.month else DISABLED,
+            command=self.create_day_button_command(day)
+        )
+        day_button.pack(side=LEFT, padx=5, pady=5)  # 버튼 간격 조정
+
+    def create_day_button_command(self, day):
+        def command():
+            self.set_selected_day(day)
+
+        return command
+
+    def set_selected_day(self, day):
+        self.day = day
+        self.selected_date_label.config(text=f"{self.day}")
+
+    def select_date(self, top):
+        try:
+            selected_date = f"{self.day.year}-{self.day.month:02}-{self.day.day:02}"
+            self.selected_date_label.config(text=f"{selected_date}")
+
+            self.year = self.day.year
+            self.month = self.day.month
+            self.day = self.day.day
+
+        except Exception as e:
+            self.selected_date_label.config(text="시작일: 없음")
+        top.destroy()
---
+++ SearchFrame.py
@@ -0,0 +1,34 @@
+from tkinter import *
+from Calender import Calender
+
+
+class SearchFrame(Frame):
+    def __init__(self, parent, main_frame):
+        super().__init__(parent)
+        self.main_frame = main_frame
+        self.year = None
+        self.month = None
+        self.day = None
+
+    def create(self):
+        self.sub_frame1 = Frame(self, bg='pink')
+        self.sub_frame2 = Frame(self, bg='light coral')
+        self.label1 = Label(self.sub_frame1, text="서브 프레임 1", font=("Arial", 16))
+        self.label2 = Label(self.sub_frame2, text="서브 프레임 2", font=("Arial", 16))
+
+        self.sub_frame1.pack(side=LEFT, fill=BOTH)
+        self.sub_frame2.pack(side=LEFT, fill=BOTH, expand=True)
+        self.label1.pack(pady=10)
+        self.label2.pack(pady=10)
+
+        self.sub_frame_top = Frame(self.sub_frame1, bg="orange")
+        self.sub_frame_top.pack(side=TOP, anchor=N, fill=X)
+
+        self.from_calender = Calender(self.sub_frame_top)
+        self.to_calender = Calender(self.sub_frame_top)
+
+    def show(self):
+        self.pack(side=RIGHT, fill=BOTH, expand=True)
+
+    def hide(self):
+        self.pack_forget()
