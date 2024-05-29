b31c6408b4ca22b09476280c7e1cb52f6593128c
2019182029_이원준 <S159753S@tukorea.ac.kr>
2024-05-29 19:22:16 +0900

    GraphFrame 수정
    - 통계 수치를 읽을 수 읽도록 수정

--- GraphFrame.py
+++ GraphFrame.py
@@ -31,10 +31,10 @@ class GraphFrame(Frame):
         self.data = None
         self.is_toggle = False
 
-        self.sub_frame_top = Frame(self, bg="orange")
-        self.sub_frame_top.grid(row=0, column=0, sticky='nsew')
+        self.sub_frame_top = Frame(self, bg='lightgray')
+        self.sub_frame_top.grid(row=0, column=0, pady=5, sticky='nsew')
 
-        self.calender_frame = Frame(self.sub_frame_top, bg='orange')
+        self.calender_frame = Frame(self.sub_frame_top, bg='lightgray')
         self.calender_frame.grid(row=0, column=0, sticky='nsew')
         self.from_calender = Calender(self.calender_frame)
         self.from_calender.date_selector_frame.grid(row=0, column=0)
@@ -44,7 +44,7 @@ class GraphFrame(Frame):
         self.mode_selector = Combobox(self.sub_frame_top, values=['지역', '장르'])
         self.mode_selector.grid(row=0, column=1)
 
-        self.checkbutton_frame = Frame(self.sub_frame_top, bg='white')
+        self.checkbutton_frame = Frame(self.sub_frame_top, bg='lightgray')
         self.checkbutton_frame.grid(row=0, column=2)
         self.nmrsVar = IntVar()
         Checkbutton(self.checkbutton_frame, text='티켓 판매', variable=self.nmrsVar, command=self.toggle_graph).grid(row=0, column=0, padx=10)
@@ -57,7 +57,7 @@ class GraphFrame(Frame):
         button.grid(row=0, column=3)
 
         self.sub_frame1 = Frame(self, bg='pink')
-        self.sub_frame1.grid(row=1, column=0, sticky='nsew')
+        self.sub_frame1.grid(row=1, column=0, pady=5, sticky='nsew')
 
         self.bar_frame = Frame(self.sub_frame1, bg='white')
         self.bar_frame.grid(row=0, column=0, sticky='nsew')
@@ -71,7 +71,7 @@ class GraphFrame(Frame):
         self.grid_propagate_configure()
 
     def show(self):
-        self.pack(side=RIGHT, fill=BOTH, expand=True)
+        self.pack(side=RIGHT, fill=BOTH, padx=5, pady=5, expand=True)
 
     def hide(self):
         self.pack_forget()
@@ -155,8 +155,38 @@ class GraphFrame(Frame):
         if self.mode:
             self.setInfo(self.mode)
 
+        if self.state == None:
+            return
+
         self.bar_canvas.create_line(50, self.bar_canvas.winfo_height() - 70, self.distance - 50, self.bar_canvas.winfo_height() - 70)
         self.bar_canvas.create_line(50, self.bar_canvas.winfo_height() - 70, 50, 50)
+        self.bar_canvas.create_line(self.distance - 50, self.bar_canvas.winfo_height() - 70, self.distance - 50, 50)
+        self.bar_canvas.create_text(50, 30, text='티켓 판매 수')
+        self.bar_canvas.create_text(self.distance - 50, 37, text='티켓 판매액')
+        self.bar_canvas.create_text(self.distance - 50, 23, text='(단위 : 억)')
+
+        gap = (self.distance - 100) // 5
+        self.bar_canvas.create_rectangle(50 + gap * 1 - 10 - 5, 30 - 5,
+                                         50 + gap * 1 - 10 + 5, 30 + 5,
+                                         fill='red')
+        self.bar_canvas.create_text(50 + gap * 1 + 45, 30, text=': 총 티켓 판매 수')
+
+        self.bar_canvas.create_rectangle(50 + gap * 2 - 10 - 5, 30 - 5,
+                                         50 + gap * 2 - 10 + 5, 30 + 5,
+                                         fill='blue')
+        self.bar_canvas.create_text(50 + gap * 2 + 35, 30, text=': 티켓 판매액')
+
+        self.bar_canvas.create_line(50 + gap * 3 - 10 - 10, 30, 50 + gap * 3 - 10 + 10, 30, width=3)
+        self.bar_canvas.create_oval(50 + gap * 3 - 10 - 5, 30 - 5,
+                                    50 + gap * 3 - 10 + 5, 30 + 5,
+                                    fill='white', width=3)
+        self.bar_canvas.create_text(50 + gap * 3 + 35, 30, text=': 개막 편수')
+
+        self.bar_canvas.create_line(50 + gap * 4 - 10 - 10, 30, 50 + gap * 4 - 10 + 10, 30, width=3, fill='gray')
+        self.bar_canvas.create_oval(50 + gap * 4 - 10 - 5, 30 - 5,
+                                    50 + gap * 4 - 10 + 5, 30 + 5,
+                                    outline='gray', fill='white', width=3)
+        self.bar_canvas.create_text(50 + gap * 4 + 35, 30, text=': 상연 횟수')
 
         if self.state != 'prfprocnt':
             self.draw_bar_graph()
@@ -174,23 +204,40 @@ class GraphFrame(Frame):
             self.bar_canvas.create_text(20 + col * self.gap, self.bar_canvas.winfo_height() - 35, text=dk)
             for k, v in dv.items():
                 if k == 'nmrs' and int(v) != 0:
-                    bh = self.get_max_value('nmrs')
-                    h = (self.bar_canvas.winfo_height() - 190) * int(v) / bh
+                    bh1 = self.get_max_value('nmrs')
+                    h = (self.bar_canvas.winfo_height() - 190) * int(v) / bh1
                     self.bar_canvas.create_rectangle(5 + col * self.gap, self.bar_canvas.winfo_height() - 70 - h,
                                                      20 + col * self.gap, self.bar_canvas.winfo_height() - 70,
                                                      fill='red')
                 if k == 'amount' and int(v) != 0:
-                    bh = self.get_max_value('amount')
-                    h = (self.bar_canvas.winfo_height() - 190) * int(v) / bh
+                    bh2 = self.get_max_value('amount')
+                    h = (self.bar_canvas.winfo_height() - 190) * int(v) / bh2
                     self.bar_canvas.create_rectangle(20 + col * self.gap, self.bar_canvas.winfo_height() - 70 - h,
                                                      35 + col * self.gap, self.bar_canvas.winfo_height() - 70,
                                                      fill='blue')
             col += 1
 
+        digit1 = 10 ** (len(str(bh1)) - 1)
+        for i in range(1, bh1 // digit1 + 1):
+            h = (self.bar_canvas.winfo_height() - 190) * int(digit1 * i) / bh1
+            self.bar_canvas.create_line(45, self.bar_canvas.winfo_height() - 70 - h,
+                                        55, self.bar_canvas.winfo_height() - 70 - h)
+            self.bar_canvas.create_text(25, self.bar_canvas.winfo_height() - 70 - h, text=str(digit1 * i))
+
+        digit2 = 10 ** (len(str(bh2)) - 2)
+        for i in range(1, bh2 // digit2 + 1):
+            if i % 5 == 0:
+                h = (self.bar_canvas.winfo_height() - 190) * int(digit2 * i) / bh2
+                self.bar_canvas.create_line(self.distance - 45, self.bar_canvas.winfo_height() - 70 - h,
+                                            self.distance - 55, self.bar_canvas.winfo_height() - 70 - h)
+                self.bar_canvas.create_text(self.distance - 25, self.bar_canvas.winfo_height() - 70 - h, text=str(digit2 * i // 100000000))
+
     def draw_line_graph(self):
         col = 1
         prfprocnt_points = []
         prfdtcnt_points = []
+        prfprocnt_values = []
+        prfdtcnt_values = []
 
         for dk, dv in self.data.items():
             if self.state == 'prfprocnt': self.bar_canvas.create_text(20 + col * self.gap, self.bar_canvas.winfo_height() - 35, text=dk)
@@ -199,10 +246,12 @@ class GraphFrame(Frame):
                     lh = self.get_max_value('prfprocnt')
                     h = (self.bar_canvas.winfo_height() - 190) * int(v) / lh
                     prfprocnt_points.append((20 + col * self.gap, self.bar_canvas.winfo_height() - 70 - h))
+                    prfprocnt_values.append(int(v))
                 if k == 'prfdtcnt':
                     lh = self.get_max_value('prfdtcnt')
                     h = (self.bar_canvas.winfo_height() - 190) * int(v) / lh
                     prfdtcnt_points.append((20 + col * self.gap, self.bar_canvas.winfo_height() - 70 - h))
+                    prfdtcnt_values.append(int(v))
             col += 1
 
         for i in range(len(prfprocnt_points)):
@@ -213,12 +262,35 @@ class GraphFrame(Frame):
                 self.bar_canvas.create_line(prfdtcnt_points[i][0], prfdtcnt_points[i][1],
                                             prfdtcnt_points[i + 1][0], prfdtcnt_points[i + 1][1],
                                             fill='gray', width=3)
+
+        for i in range(len(prfprocnt_points)):
             self.bar_canvas.create_oval(prfprocnt_points[i][0] - 5, prfprocnt_points[i][1] - 5,
                                         prfprocnt_points[i][0] + 5, prfprocnt_points[i][1] + 5,
                                         fill='white', width=3)
             self.bar_canvas.create_oval(prfdtcnt_points[i][0] - 5, prfdtcnt_points[i][1] - 5,
                                         prfdtcnt_points[i][0] + 5, prfdtcnt_points[i][1] + 5,
                                         outline='gray', fill='white', width=3)
+
+            if (prfprocnt_points[i][1] >= prfdtcnt_points[i][1]): g1, g2 = 19, -17
+            else: g1, g2 = -17, 19
+
+            if prfprocnt_values[i] != 0:
+                for dx in range(-2, 2 + 1):
+                    for dy in range(-2, 2 + 1):
+                        if dx != 0 or dy != 0:
+                            self.bar_canvas.create_text(prfprocnt_points[i][0] + dx, prfprocnt_points[i][1] + g1 + dy,
+                                                        text=str(prfprocnt_values[i]), font=('arial', 15, 'bold'))
+                self.bar_canvas.create_text(prfprocnt_points[i][0], prfprocnt_points[i][1] + g1,
+                                            text=str(prfprocnt_values[i]), font=('arial', 15, 'bold'), fill='white')
+            if prfdtcnt_values[i] != 0:
+                for dx in range(-2, 2 + 1):
+                    for dy in range(-2, 2 + 1):
+                        if dx != 0 or dy != 0:
+                            self.bar_canvas.create_text(prfdtcnt_points[i][0] + dx, prfdtcnt_points[i][1] + g2 + dy,
+                                                        text=str(prfdtcnt_values[i]), font=('arial', 15, 'bold'),
+                                                        fill='gray')
+                self.bar_canvas.create_text(prfdtcnt_points[i][0], prfdtcnt_points[i][1] + g2,
+                                            text=str(prfdtcnt_values[i]), font=('arial', 15, 'bold'), fill='white')
 
     def draw_circular_graph(self):
         shr = []
